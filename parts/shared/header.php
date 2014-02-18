c3a14b98009ac423344fbd8246fbfd1507bf2ee7
bbowen <brett.bowen@ksuwdc.com>
2014-02-17 21:06:25 -0500

    Playing with new word press design

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,5 +1,5 @@
 <header>
-	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+	<h1><a href="<?php echo home_url(); ?>"><img src="http://bbowen.ksuwdc.com/starkers/parts/shared/2014/02/ardy-150x150.jpg"></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
 </header>
