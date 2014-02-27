3e367830618aa03326085074e1e379ae78d41a36
bbowen <brett.bowen@ksuwdc.com>
2014-02-26 19:15:33 -0500

    playing with nothing

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -2,4 +2,5 @@
 	<h1><a href="<?php echo home_url(); ?>"><img src="http://bbowen.ksuwdc.com/starkers/parts/shared/2014/02/ardy-150x150.jpg"></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
+	where are you 
 </header>
